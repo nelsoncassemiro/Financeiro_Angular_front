c1462ed235b2186c7ba949a7f896b83a106f29b8
Nelson Cassemiro <nelsoncassemiro@gmail.com>
2024-05-12 15:36:44 -0300

    CRIANDO TELA DE LOGIM COM HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(public formBuilder: FormBuilder,
+    private router: Router){
+
+  }
+
+  loginForm: FormGroup;
+
+  ngOnInit():void
+  {
+    this.loginForm = this.formBuilder.group
+    (
+      {
+        email : ['',[Validators.required, Validators.email]],
+        senha : ['',[Validators.required]]
+      }
+    )
+  }
+
+  get dadosForm()
+  {
+    return this, this.loginForm.controls;
+  }
+
+  loginUser()
+  {
+    alert("Ok");
+  }
 }
